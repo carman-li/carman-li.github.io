8642e456f196dbcf95096869843ffce593f94378
Carman Li <licarman6962@gmail.com>
2019-12-04 22:20:42 -0500

    wip

--- index/index.js
+++ index/index.js
@@ -1,7 +1,16 @@
-$(function () {
-    $('.header').height($(window).height());
+$(document).ready(function () {
+    $(function () {
+        $("#navigation").load("https://carman-li.github.io/navbar/navbar.html")
+    });
 });
 
-$(function () {
-    $("#navigation").load("https://carman-li.github.io/navbar/navbar.html")
+
+// $(document).ready(function () {
+//     $('#toggler').click(function () {
+//         $('#navigation').load("https://carman-li.github.io/navbar/navbar.html");
+//     })
+// });
+
+$("#toggler").on("click", function () {
+    $('#navigation').load("https://carman-li.github.io/navbar/navbar.html");
 });
--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,5 +1,16 @@
-$(function () {
-    $("#toggler").click(function () {
+$(document).ready(function () {
+    $(function () {
         $("#navigation").load("https://carman-li.github.io/navbar/navbar.html")
     });
+});
+
+
+// $(document).ready(function () {
+//     $('#toggler').click(function () {
+//         $('#navigation').load("https://carman-li.github.io/navbar/navbar.html");
+//     })
+// });
+
+$("#toggler").on("click", function () {
+    $('#navigation').load("https://carman-li.github.io/navbar/navbar.html");
 });
