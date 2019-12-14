692a2e036d27ea915e6d800447bac09446a234b8
Carman Li <licarman6962@gmail.com>
2019-12-14 01:48:18 -0500

    about page background; integrating navbar

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
-    $(function () {
-        $("#navigation").load("https://carman-li.github.io/navbar/navbar.html")
-    });
-});
+// $(document).ready(function () {
+//     $(function () {
+//         $("#navigation").load("https://carman-li.github.io/navbar/navbar.html")
+//     });
+// });
 
-$("#toggler").on("click", function () {
-    $('#navigation').load("https://carman-li.github.io/navbar/navbar.html");
-});
+// $("#toggler").on("click", function () {
+//     $('#navigation').load("https://carman-li.github.io/navbar/navbar.html");
+// });
