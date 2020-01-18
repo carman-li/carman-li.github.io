4ef0772606fa04b7a1c7b9eaa5f96f17889e0871
Carman Li <licarman6962@gmail.com>
2020-01-18 01:20:36 -0500

    updates

---
+++ happy/happy.js
@@ -0,0 +1,12 @@
+$(function() {
+    $("#header").load("../navbar/navbar.html");
+});
+
+$(function() {
+    $("#footer").load("../footer/footer.html");
+});
+
+$(function() {
+    $('body').removeClass('fade-out');
+    $('body').addClass("background");
+});
