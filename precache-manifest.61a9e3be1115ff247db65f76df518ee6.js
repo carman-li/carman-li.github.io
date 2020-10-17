9e3e236fdc38f8b1d859bf9db51ef387b51e89a5
Carman Li <carman@gomarketbox.com>
2020-10-17 05:23:45 -0400

    Updates

---
+++ precache-manifest.61a9e3be1115ff247db65f76df518ee6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cee5733f3447bcb5c7a8c75b42cada2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a35380ffd8652ca4127",
+    "url": "/static/css/2.5438e6ee.chunk.css"
+  },
+  {
+    "revision": "4084386052fd34a4e40e",
+    "url": "/static/css/main.6ae905c2.chunk.css"
+  },
+  {
+    "revision": "9a35380ffd8652ca4127",
+    "url": "/static/js/2.2e20adee.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.2e20adee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4084386052fd34a4e40e",
+    "url": "/static/js/main.11226e12.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "0072d43bf4c811b50ed285ba22adf5c5",
+    "url": "/static/media/logo.0072d43b.png"
+  },
+  {
+    "revision": "72451ed306860aeafef844e1934da48c",
+    "url": "/static/media/slice.72451ed3.png"
+  },
+  {
+    "revision": "cedd0d008656de56ea85caf50e418f01",
+    "url": "/static/media/yelpRoulette.cedd0d00.jpg"
+  }
+]);
